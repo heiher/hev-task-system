045f8a8389b5945f3e688fd59c2ecc7136114399
Heiher <r@hev.cc>
2017-02-18 13:48:11 +0800

    Apps: Timeout: Using task_sleep.

--- apps/timeout.c
+++ apps/timeout.c
@@ -8,10 +8,6 @@
  */
 
 #include <stdio.h>
-#include <stdint.h>
-#include <errno.h>
-#include <unistd.h>
-#include <sys/timerfd.h>
 
 #include <hev-task.h>
 #include <hev-task-system.h>
@@ -19,44 +15,11 @@
 static void
 task_entry (void *data)
 {
-	HevTask *task = hev_task_self ();
-	int fd;
-	struct itimerspec spec;
 	const unsigned int interval = 1000;
-	ssize_t size;
-	uint64_t time;
-
-	fd = timerfd_create (CLOCK_MONOTONIC, TFD_NONBLOCK);
-	if (fd == -1) {
-		fprintf (stderr, "Create timer fd failed!\n");
-		return;
-	}
-
-	hev_task_add_fd (task, fd, EPOLLIN);
-
-	spec.it_interval.tv_sec = 0;
-	spec.it_interval.tv_nsec = 0;
-	spec.it_value.tv_sec = interval / 1000;
-	spec.it_value.tv_nsec = (interval % 1000) * 1000 * 1000;
-	timerfd_settime (fd, 0, &spec, NULL);
 
 	printf ("waiting for timeout %ums ...\n", interval);
-retry:
-	size = read (fd, &time, sizeof (time));
-	if (size == -1) {
-		if (errno == EAGAIN) {
-			hev_task_yield (HEV_TASK_WAITIO);
-			goto retry;
-		}
-
-		close (fd);
-		fprintf (stderr, "Read timer fd failed!\n");
-		return;
-	}
-
+	hev_task_sleep (interval);
 	printf ("timeout\n");
-
-	close (fd);
 }
 
 int
