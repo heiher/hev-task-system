4c70662edb091a11f1f738a9b3c23be289ccaa6d
Heiher <r@hev.cc>
2017-02-22 20:08:26 +0800

    HevTaskSystem: Import annotations.

--- src/hev-task-system.h
+++ src/hev-task-system.h
@@ -10,8 +10,33 @@
 #ifndef __HEV_TASK_SYSTEM_H__
 #define __HEV_TASK_SYSTEM_H__
 
+/**
+ * hev_task_system_init:
+ *
+ * Initialize the task system.
+ *
+ * Returns: When successful, returns zero. When an error occurs, returns -1.
+ *
+ * Since: 1.0
+ */
 int hev_task_system_init (void);
+
+/**
+ * hev_task_system_fini:
+ *
+ * Finalize the task system.
+ *
+ * Since: 1.0
+ */
 void hev_task_system_fini (void);
+
+/**
+ * hev_task_system_run:
+ *
+ * Run the task system.
+ *
+ * Since: 1.0
+ */
 void hev_task_system_run (void);
 
 #endif /* __HEV_TASK_SYSTEM_H__ */
