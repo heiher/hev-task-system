3a21127028a13f72aa7ec5c47a9a81f608fdf4fe
Heiher <r@hev.cc>
2018-09-05 20:51:26 +0800

    HevTask: Import task execute for riscv64.

---
+++ src/hev-task-execute-riscv64.s
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : hev-task-execute-riscv64.s
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description :
+ ============================================================================
+ */
+
+	.globl  hev_task_execute
+	.type   hev_task_execute, @function
+
+hev_task_execute:
+	ld	t0, (a0)
+	sd	sp, -0x08(t0)
+	sd	ra, -0x10(t0)
+	addi	sp, t0, -0x10
+
+	jalr	a1
+
+	ld	ra, (sp)
+	ld	sp, 0x08(sp)
+	jr	ra
+
+	.size   hev_task_execute, . - hev_task_execute
+
