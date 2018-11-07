f8a7c32fc4392a14c88da377a42edd2b1cf8fc0b
Heiher <r@hev.cc>
2018-11-07 22:13:24 +0800

    HevMemoryAllocatorSimple: Init.

---
+++ src/hev-memory-allocator-simple.c
@@ -0,0 +1,45 @@
+/*
+ ============================================================================
+ Name        : hev-memory-allocator-simple.c
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Memory allocator simple
+ ============================================================================
+ */
+
+#include <stdlib.h>
+
+#include "hev-memory-allocator-simple.h"
+
+static void *_hev_memory_allocator_alloc (HevMemoryAllocator *self,
+                                          size_t size);
+static void _hev_memory_allocator_free (HevMemoryAllocator *self, void *ptr);
+
+HevMemoryAllocator *
+hev_memory_allocator_simple_new (void)
+{
+    HevMemoryAllocator *self = NULL;
+
+    self = malloc (sizeof (HevMemoryAllocator));
+    if (!self)
+        return NULL;
+
+    self->ref_count = 1;
+    self->alloc = _hev_memory_allocator_alloc;
+    self->free = _hev_memory_allocator_free;
+    self->destroy = NULL;
+
+    return self;
+}
+
+static void *
+_hev_memory_allocator_alloc (HevMemoryAllocator *self, size_t size)
+{
+    return malloc (size);
+}
+
+static void
+_hev_memory_allocator_free (HevMemoryAllocator *self, void *ptr)
+{
+    free (ptr);
+}
---
+++ src/hev-memory-allocator-simple.h
@@ -0,0 +1,26 @@
+/*
+ ============================================================================
+ Name        : hev-memory-allocator-simple.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Memory allocator simple
+ ============================================================================
+ */
+
+#ifndef __HEV_MEMORY_ALLOCATOR_SIMPLE_H__
+#define __HEV_MEMORY_ALLOCATOR_SIMPLE_H__
+
+#include "hev-memory-allocator.h"
+
+/**
+ * hev_memory_allocator_simple_new:
+ *
+ * Creates a new simple memory allocator.
+ *
+ * Returns: a new #HevMemoryAllocator
+ *
+ * Since: 3.2
+ */
+HevMemoryAllocator *hev_memory_allocator_simple_new (void);
+
+#endif /* __HEV_MEMORY_ALLOCATOR_SIMPLE_H__ */
