17ec00810f9465ec0059bbe248f51d5456dbbe07
Heiher <r@hev.cc>
2018-12-05 20:17:45 +0800

    Tests: Poll: Use task I/O poll.

--- tests/io-poll.c
+++ tests/io-poll.c
@@ -15,14 +15,14 @@
 #include <hev-task.h>
 #include <hev-task-system.h>
 #include <hev-task-io-pipe.h>
-#include <hev-task-poll.h>
+#include <hev-task-io-poll.h>
 
 static void
 task_entry (void *data)
 {
     int fds[2];
     int val;
-    HevTaskPollFD pfds[2];
+    HevTaskIOPollFD pfds[2];
 
     assert (hev_task_io_pipe_pipe (fds) == 0);
     assert (fds[0] >= 0);
@@ -35,7 +35,7 @@ task_entry (void *data)
     pfds[1].fd = fds[1];
     pfds[1].events = POLLOUT;
 
-    val = hev_task_poll (pfds, 2, -1);
+    val = hev_task_io_poll (pfds, 2, -1);
     assert (val == 2);
     assert (pfds[0].revents & POLLIN);
     assert (pfds[1].revents & POLLOUT);
