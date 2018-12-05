afa9438ecb1c4b0edc571789a3de9660ea68628e
Heiher <r@hev.cc>
2018-12-05 21:33:40 +0800

    Tests: Poll: Test cover fast and slow path.

--- tests/io-poll.c
+++ tests/io-poll.c
@@ -17,31 +17,38 @@
 #include <hev-task-io-pipe.h>
 #include <hev-task-io-poll.h>
 
+static int fds[2];
+
 static void
-task_entry (void *data)
+task1_entry (void *data)
 {
-    int fds[2];
     int val;
-    HevTaskIOPollFD pfds[2];
-
-    assert (hev_task_io_pipe_pipe (fds) == 0);
-    assert (fds[0] >= 0);
-    assert (fds[1] >= 0);
-
-    assert (write (fds[1], &val, sizeof (val)) == sizeof (val));
+    HevTaskIOPollFD pfds[1];
 
     pfds[0].fd = fds[0];
     pfds[0].events = POLLIN;
-    pfds[1].fd = fds[1];
-    pfds[1].events = POLLOUT;
 
-    val = hev_task_io_poll (pfds, 2, -1);
-    assert (val == 2);
+    val = hev_task_io_poll (pfds, 1, -1);
+    assert (val == 1);
     assert (pfds[0].revents & POLLIN);
-    assert (pfds[1].revents & POLLOUT);
+}
 
-    close (fds[0]);
-    close (fds[1]);
+static void
+task2_entry (void *data)
+{
+    int val;
+    HevTaskIOPollFD pfds[1];
+
+    hev_task_sleep (50);
+
+    pfds[0].fd = fds[1];
+    pfds[0].events = POLLOUT;
+
+    val = hev_task_io_poll (pfds, 1, -1);
+    assert (val == 1);
+    assert (pfds[0].revents & POLLOUT);
+
+    assert (write (fds[1], &val, sizeof (val)) == sizeof (val));
 }
 
 int
@@ -51,11 +58,22 @@ main (int argc, char *argv[])
 
     assert (hev_task_system_init () == 0);
 
+    assert (hev_task_io_pipe_pipe (fds) == 0);
+    assert (fds[0] >= 0);
+    assert (fds[1] >= 0);
+
     task = hev_task_new (-1);
     assert (task);
-    hev_task_run (task, task_entry, NULL);
+    hev_task_run (task, task1_entry, NULL);
+
+    task = hev_task_new (-1);
+    assert (task);
+    hev_task_run (task, task2_entry, NULL);
 
     hev_task_system_run ();
+
+    close (fds[0]);
+    close (fds[1]);
 
     hev_task_system_fini ();
 
