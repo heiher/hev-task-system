fab6aba836c5656afca607e03aa939d8d17740ed
Heiher <r@hev.cc>
2018-12-05 13:09:41 +0800

    HevTaskIOReactorEPoll: Init.

---
+++ src/kern/io/hev-task-io-reactor-epoll.c
@@ -0,0 +1,79 @@
+/*
+ ============================================================================
+ Name        : hev-task-io-reactor-epoll.c
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : I/O Reactor EPoll
+ ============================================================================
+ */
+
+#if defined(__linux__)
+
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "kern/io/hev-task-io-reactor.h"
+#include "mm/api/hev-memory-allocator-api.h"
+
+HevTaskIOReactor *
+hev_task_io_reactor_new (void)
+{
+    HevTaskIOReactor *self;
+    int flags;
+
+    self = hev_malloc0 (sizeof (HevTaskIOReactor));
+    if (!self)
+        return NULL;
+
+    self->fd = epoll_create (128);
+    if (self->fd == -1) {
+        hev_free (self);
+        return NULL;
+    }
+
+    flags = fcntl (self->fd, F_GETFD);
+    if (flags == -1) {
+        hev_free (self);
+        return NULL;
+    }
+
+    flags |= FD_CLOEXEC;
+    if (fcntl (self->fd, F_SETFD, flags) == -1) {
+        hev_free (self);
+        return NULL;
+    }
+
+    return self;
+}
+
+void
+hev_task_io_reactor_destroy (HevTaskIOReactor *self)
+{
+    close (self->fd);
+    hev_free (self);
+}
+
+int
+hev_task_io_reactor_setup (HevTaskIOReactor *self,
+                           HevTaskIOReactorSetupEvent *events, int count)
+{
+    int i, res = 0;
+
+    for (i = 0; i < count; i++) {
+        HevTaskIOReactorSetupEvent *ev = &events[i];
+        ev->event.events |= EPOLLET;
+        res |= epoll_ctl (self->fd, ev->op, ev->fd, &ev->event);
+    }
+
+    return res;
+}
+
+int
+hev_task_io_reactor_wait (HevTaskIOReactor *self,
+                          HevTaskIOReactorWaitEvent *events, int count,
+                          int timeout)
+{
+    return epoll_wait (self->fd, events, count, timeout);
+}
+
+#endif /* defined(__linux__) */
---
+++ src/kern/io/hev-task-io-reactor-epoll.h
@@ -0,0 +1,69 @@
+/*
+ ============================================================================
+ Name        : hev-task-io-reactor-epoll.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : I/O Reactor EPoll
+ ============================================================================
+ */
+
+#ifndef __HEV_TASK_IO_REACTOR_EPOLL_H__
+#define __HEV_TASK_IO_REACTOR_EPOLL_H__
+
+#include <sys/epoll.h>
+
+typedef struct _HevTaskIOReactorSetupEvent HevTaskIOReactorSetupEvent;
+typedef struct epoll_event HevTaskIOReactorWaitEvent;
+
+enum _HevTaskIOReactorEvents
+{
+    HEV_TASK_IO_REACTOR_EV_RO = EPOLLIN,
+    HEV_TASK_IO_REACTOR_EV_WO = EPOLLOUT,
+    HEV_TASK_IO_REACTOR_EV_RW = EPOLLIN | EPOLLOUT,
+};
+
+enum _HevTaskIOReactorOperation
+{
+    HEV_TASK_IO_REACTOR_OP_ADD = EPOLL_CTL_ADD,
+    HEV_TASK_IO_REACTOR_OP_MOD = EPOLL_CTL_MOD,
+    HEV_TASK_IO_REACTOR_OP_DEL = EPOLL_CTL_DEL,
+};
+
+struct _HevTaskIOReactorSetupEvent
+{
+    HevTaskIOReactorOperation op;
+
+    int fd;
+    struct epoll_event event;
+};
+
+static inline int
+hev_task_io_reactor_get_oneshot (void)
+{
+    return 0;
+}
+
+static inline void
+hev_task_io_reactor_setup_event_set (HevTaskIOReactorSetupEvent *event, int fd,
+                                     HevTaskIOReactorOperation op,
+                                     unsigned int events, void *data)
+{
+    event->op = op;
+    event->fd = fd;
+    event->event.events = events;
+    event->event.data.ptr = data;
+}
+
+static inline unsigned int
+hev_task_io_reactor_wait_event_get_events (HevTaskIOReactorWaitEvent *event)
+{
+    return event->events;
+}
+
+static inline void *
+hev_task_io_reactor_wait_event_get_data (HevTaskIOReactorWaitEvent *event)
+{
+    return event->data.ptr;
+}
+
+#endif /* __HEV_TASK_IO_REACTOR_EPOLL_H__ */
