dd970bbe2a6bfb59d0c8c273dde67fcc93c0dcee
Heiher <r@hev.cc>
2018-12-05 13:10:46 +0800

    HevTaskTimerKEvent: Init.

---
+++ src/kern/time/hev-task-timer-kevent.c
@@ -0,0 +1,33 @@
+/*
+ ============================================================================
+ Name        : hev-task-timer-kevent.c
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Timer KEvent
+ ============================================================================
+ */
+
+#if !defined(__linux__)
+
+#include "hev-task-timer-kevent.h"
+#include "mm/api/hev-memory-allocator-api.h"
+
+HevTaskTimer *
+hev_task_timer_new (void)
+{
+    HevTaskTimer *self;
+
+    self = hev_malloc0 (sizeof (HevTaskTimer));
+    if (!self)
+        return NULL;
+
+    return self;
+}
+
+void
+hev_task_timer_destroy (HevTaskTimer *self)
+{
+    hev_free (self);
+}
+
+#endif /* !defined(__linux__) */
---
+++ src/kern/time/hev-task-timer-kevent.h
@@ -0,0 +1,83 @@
+/*
+ ============================================================================
+ Name        : hev-task-timer-kevent.h
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2018 everyone.
+ Description : Timer KEvent
+ ============================================================================
+ */
+
+#ifndef __HEV_TASK_TIMER_KEVENT_H__
+#define __HEV_TASK_TIMER_KEVENT_H__
+
+#include <time.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/event.h>
+#include <sys/time.h>
+
+#include "hev-task-timer.h"
+#include "kern/core/hev-task-system-private.h"
+
+static inline int
+hev_task_timer_fast_check (HevTaskTimer *self)
+{
+    return 0;
+}
+
+static inline int
+hev_task_timer_set_time (HevTaskTimer *self, const struct timespec *expire)
+{
+    HevTaskIOReactor *reactor;
+    struct kevent event;
+    struct timespec curr;
+    time_t sec;
+    long nsec;
+    int fd;
+
+    if (clock_gettime (CLOCK_MONOTONIC, &curr) == -1)
+        abort ();
+
+    sec = expire->tv_sec - curr.tv_sec;
+    nsec = expire->tv_nsec - curr.tv_nsec;
+    if (nsec < 0) {
+        sec--;
+        nsec += 1000000000L;
+    }
+    nsec += sec * 1000000000L;
+
+    reactor = hev_task_system_get_context ()->reactor;
+    fd = hev_task_io_reactor_get_fd (reactor);
+
+    EV_SET (&event, (uintptr_t)self, EVFILT_TIMER, EV_DELETE, 0, 0, 0);
+    kevent (fd, &event, 1, NULL, 0, NULL);
+
+    EV_SET (&event, (uintptr_t)self, EVFILT_TIMER, EV_ADD | EV_ONESHOT,
+            NOTE_NSECONDS, nsec, &self->sched_entity);
+    return kevent (fd, &event, 1, NULL, 0, NULL);
+}
+
+static inline unsigned int
+hev_task_timer_get_time (HevTaskTimer *self, const struct timespec *expire)
+{
+    struct timespec curr;
+    time_t sec;
+    long nsec;
+
+    if (clock_gettime (CLOCK_MONOTONIC, &curr) == -1)
+        abort ();
+
+    sec = expire->tv_sec - curr.tv_sec;
+    nsec = expire->tv_nsec - curr.tv_nsec;
+    if (nsec < 0) {
+        sec--;
+        nsec += 1000000000L;
+    }
+
+    if (sec < 0)
+        return 0;
+
+    return (sec * 1000000) + (nsec / 1000);
+}
+
+#endif /* __HEV_TASK_TIMER_KEVENT_H__ */
