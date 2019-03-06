a10418f0ba463139577e525586d611f7cd1a4d43
Heiher <r@hev.cc>
2019-03-06 20:53:03 +0800

    HevTaskIOSocket: Remove resume fd events.

--- src/lib/io/socket/hev-task-io-socket.c
+++ src/lib/io/socket/hev-task-io-socket.c
@@ -14,7 +14,6 @@
 
 #include "kern/task/hev-task.h"
 #include "lib/io/basic/hev-task-io.h"
-#include "lib/io/basic/hev-task-io-shared.h"
 #include "hev-task-io-socket.h"
 
 int
@@ -68,7 +67,6 @@ retry:
     ret = connect (fd, addr, addr_len);
     if (ret == -1) {
         if (errno == EINPROGRESS || errno == EALREADY) {
-            hev_task_io_res_fd (fd, HEV_TASK_IO_REACTOR_EV_WO);
             if (yielder) {
                 if (yielder (HEV_TASK_WAITIO, yielder_data))
                     return -2;
@@ -93,7 +91,6 @@ hev_task_io_socket_accept (int fd, struct sockaddr *addr, socklen_t *addr_len,
 retry:
     new_fd = accept (fd, addr, addr_len);
     if (new_fd == -1 && errno == EAGAIN) {
-        hev_task_io_res_fd (fd, HEV_TASK_IO_REACTOR_EV_RO);
         if (yielder) {
             if (yielder (HEV_TASK_WAITIO, yielder_data))
                 return -2;
@@ -121,7 +118,6 @@ hev_task_io_socket_recv (int fd, void *buf, size_t len, int flags,
 retry:
     s = recv (fd, buf + size, len - size, flags & ~MSG_WAITALL);
     if (s == -1 && errno == EAGAIN) {
-        hev_task_io_res_fd (fd, HEV_TASK_IO_REACTOR_EV_RO);
         if (yielder) {
             if (yielder (HEV_TASK_WAITIO, yielder_data))
                 return -2;
@@ -154,7 +150,6 @@ hev_task_io_socket_send (int fd, const void *buf, size_t len, int flags,
 retry:
     s = send (fd, buf + size, len - size, flags & ~MSG_WAITALL);
     if (s == -1 && errno == EAGAIN) {
-        hev_task_io_res_fd (fd, HEV_TASK_IO_REACTOR_EV_WO);
         if (yielder) {
             if (yielder (HEV_TASK_WAITIO, yielder_data))
                 return -2;
@@ -187,7 +182,6 @@ hev_task_io_socket_recvfrom (int fd, void *buf, size_t len, int flags,
 retry:
     s = recvfrom (fd, buf, len, flags, addr, addr_len);
     if (s == -1 && errno == EAGAIN) {
-        hev_task_io_res_fd (fd, HEV_TASK_IO_REACTOR_EV_RO);
         if (yielder) {
             if (yielder (HEV_TASK_WAITIO, yielder_data))
                 return -2;
@@ -210,7 +204,6 @@ hev_task_io_socket_sendto (int fd, const void *buf, size_t len, int flags,
 retry:
     s = sendto (fd, buf, len, flags, addr, addr_len);
     if (s == -1 && errno == EAGAIN) {
-        hev_task_io_res_fd (fd, HEV_TASK_IO_REACTOR_EV_WO);
         if (yielder) {
             if (yielder (HEV_TASK_WAITIO, yielder_data))
                 return -2;
@@ -248,7 +241,6 @@ hev_task_io_socket_recvmsg (int fd, struct msghdr *msg, int flags,
 retry:
     s = recvmsg (fd, &mh, flags & ~MSG_WAITALL);
     if (s == -1 && errno == EAGAIN) {
-        hev_task_io_res_fd (fd, HEV_TASK_IO_REACTOR_EV_RO);
         if (yielder) {
             if (yielder (HEV_TASK_WAITIO, yielder_data))
                 return -2;
@@ -310,7 +302,6 @@ hev_task_io_socket_sendmsg (int fd, const struct msghdr *msg, int flags,
 retry:
     s = sendmsg (fd, &mh, flags & ~MSG_WAITALL);
     if (s == -1 && errno == EAGAIN) {
-        hev_task_io_res_fd (fd, HEV_TASK_IO_REACTOR_EV_WO);
         if (yielder) {
             if (yielder (HEV_TASK_WAITIO, yielder_data))
                 return -2;
