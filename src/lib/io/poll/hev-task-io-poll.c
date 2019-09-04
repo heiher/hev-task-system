437e81f81979f71fcb321cf686fd630ece30892c
Heiher <r@hev.cc>
2019-09-05 00:13:12 +0800

    HevTaskIOPoll: Refactor poll.

--- src/lib/io/poll/hev-task-io-poll.c
+++ src/lib/io/poll/hev-task-io-poll.c
@@ -2,7 +2,7 @@
  ============================================================================
  Name        : hev-task-io-poll.c
  Author      : Heiher <r@hev.cc>
- Copyright   : Copyright (c) 2017 - 2018 everyone.
+ Copyright   : Copyright (c) 2017 - 2019 everyone.
  Description :
  ============================================================================
  */
@@ -15,37 +15,31 @@
 EXPORT_SYMBOL int
 hev_task_io_poll (HevTaskIOPollFD fds[], unsigned int nfds, int timeout)
 {
-    HevTask *task;
-    unsigned int i;
-    int ret;
+    int res;
 
-    ret = poll (fds, nfds, 0);
-    if ((ret > 0) || (timeout == 0))
-        return ret;
+    if (timeout == 0) {
+        res = poll (fds, nfds, 0);
+    } else {
+        HevTask *task = hev_task_self ();
+        unsigned int i;
 
-    task = hev_task_self ();
-    for (i = 0; i < nfds; i++) {
-        if (hev_task_mod_fd (task, fds[i].fd, fds[i].events) == -1)
-            hev_task_add_fd (task, fds[i].fd, fds[i].events);
+        for (i = 0; i < nfds; i++) {
+            if (hev_task_mod_fd (task, fds[i].fd, fds[i].events) < 0)
+                hev_task_add_fd (task, fds[i].fd, fds[i].events);
+        }
+
+        if (timeout > 0) {
+            do {
+                timeout = hev_task_sleep (timeout);
+                res = poll (fds, nfds, 0);
+            } while (timeout > 0 && res == 0);
+        } else {
+            do {
+                hev_task_yield (HEV_TASK_WAITIO);
+                res = poll (fds, nfds, 0);
+            } while (res == 0);
+        }
     }
 
-    if (timeout > 0) {
-    retry_sleep:
-        timeout = hev_task_sleep (timeout);
-        ret = poll (fds, nfds, 0);
-        if (timeout > 0 && ret == 0)
-            goto retry_sleep;
-
-        goto quit;
-    }
-
-retry:
-    ret = poll (fds, nfds, 0);
-    if (ret == 0) {
-        hev_task_yield (HEV_TASK_WAITIO);
-        goto retry;
-    }
-
-quit:
-    return ret;
+    return res;
 }
