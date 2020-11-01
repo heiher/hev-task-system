8f945458027dfa0e26b4740d33bce5f42c1186a8
Heiher <r@hev.cc>
2020-11-01 11:32:12 +0800

    HevTask: Import task execute for loong.

---
+++ src/arch/loong/hev-task-execute-la32.s
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : hev-task-execute-la32.s
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2020 everyone.
+ Description :
+ ============================================================================
+ */
+
+    .globl hev_task_execute
+    .ent hev_task_execute, 0
+    .type hev_task_execute, @function
+
+hev_task_execute:
+    ld.w  $t0, $a0, 0
+    st.w  $sp, $t0, -4
+    st.w  $ra, $t0, -8
+    addi.w  $sp, $t0, -8
+
+    jirl  $ra, $a1, 0
+
+    ld.w  $ra, $sp, 0
+    ld.w  $sp, $sp, 4
+    jirl  $zero, $ra, 0
+
+    .end hev_task_execute
+    .size hev_task_execute, . - hev_task_execute
---
+++ src/arch/loong/hev-task-execute-la64.s
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : hev-task-execute-la64.s
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2020 everyone.
+ Description :
+ ============================================================================
+ */
+
+    .globl hev_task_execute
+    .ent hev_task_execute, 0
+    .type hev_task_execute, @function
+
+hev_task_execute:
+    ld.d  $t0, $a0, 0
+    st.d  $sp, $t0, -8
+    st.d  $ra, $t0, -16
+    addi.d  $sp, $t0, -16
+
+    jirl  $ra, $a1, 0
+
+    ld.d  $ra, $sp, 0
+    ld.d  $sp, $sp, 8
+    jirl  $zero, $ra, 0
+
+    .end hev_task_execute
+    .size hev_task_execute, . - hev_task_execute
