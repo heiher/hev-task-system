51209f04fa8a8256eef06ec018030479ae20b5af
Heiher <r@hev.cc>
2020-11-20 21:29:19 +0800

    HevTaskSystem: Refactor.

--- src/kern/core/hev-task-system.c
+++ src/kern/core/hev-task-system.c
@@ -2,16 +2,12 @@
  ============================================================================
  Name        : hev-task-system.c
  Author      : Heiher <r@hev.cc>
- Copyright   : Copyright (c) 2017 everyone.
+ Copyright   : Copyright (c) 2017 - 2020 everyone.
  Description :
  ============================================================================
  */
 
 #include <stdlib.h>
-
-#ifdef ENABLE_PTHREAD
-#include <pthread.h>
-#endif
 
 #include "lib/misc/hev-compiler.h"
 #include "mem/api/hev-memory-allocator-api.h"
@@ -22,85 +18,110 @@
 #include "hev-task-system.h"
 
 #ifdef ENABLE_PTHREAD
+
+#include <pthread.h>
+
 static pthread_key_t key;
 static pthread_once_t key_once = PTHREAD_ONCE_INIT;
 
-static void pthread_key_creator (void);
-#else
+static void
+pthread_key_creator (void)
+{
+    pthread_key_create (&key, NULL);
+}
+
+HevTaskSystemContext *
+hev_task_system_get_context (void)
+{
+    return pthread_getspecific (key);
+}
+
+static inline int
+hev_task_system_set_context (HevTaskSystemContext *context)
+{
+    return pthread_setspecific (key, context);
+}
+
+#else /* !ENABLE_PTHREAD */
+
 static HevTaskSystemContext *default_context;
-#endif
+
+HevTaskSystemContext *
+hev_task_system_get_context (void)
+{
+    return default_context;
+}
+
+static inline int
+hev_task_system_set_context (HevTaskSystemContext *context)
+{
+    default_context = context;
+    return 0;
+}
+
+#endif /* !ENABLE_PTHREAD */
 
 EXPORT_SYMBOL int
 hev_task_system_init (void)
 {
-#ifdef ENABLE_MEMALLOC_SLICE
-    HevMemoryAllocator *allocator;
-#endif
-#ifdef ENABLE_PTHREAD
-    HevTaskSystemContext *default_context;
-#endif
+    HevMemoryAllocator *allocator = NULL;
+    HevTaskSystemContext *context;
 
 #ifdef ENABLE_MEMALLOC_SLICE
     allocator = hev_memory_allocator_slice_new ();
-    if (allocator) {
-        allocator = hev_memory_allocator_set_default (allocator);
-        if (allocator)
-            hev_memory_allocator_unref (allocator);
-    }
 #endif
+    allocator = hev_memory_allocator_set_default (allocator);
+    if (allocator)
+        hev_memory_allocator_unref (allocator);
 
 #ifdef ENABLE_PTHREAD
     pthread_once (&key_once, pthread_key_creator);
-    default_context = pthread_getspecific (key);
 #endif
 
-    if (default_context)
-        return -1;
+    if (hev_task_system_get_context ())
+        goto exit;
 
-    default_context = hev_malloc0 (sizeof (HevTaskSystemContext));
-    if (!default_context)
-        return -2;
+    context = hev_malloc0 (sizeof (HevTaskSystemContext));
+    if (!context)
+        goto exit;
 
-#ifdef ENABLE_PTHREAD
-    pthread_setspecific (key, default_context);
-#endif
+    if (hev_task_system_set_context (context) < 0)
+        goto free_context;
 
-    default_context->reactor = hev_task_io_reactor_new ();
-    if (!default_context->reactor)
-        return -3;
+    context->reactor = hev_task_io_reactor_new ();
+    if (!context->reactor)
+        goto rest_context;
 
-    default_context->timer = hev_task_timer_new ();
-    if (!default_context->timer)
-        return -4;
+    context->timer = hev_task_timer_new ();
+    if (!context->timer)
+        goto free_reactor;
 
     return 0;
+
+free_reactor:
+    hev_task_io_reactor_destroy (context->reactor);
+rest_context:
+    hev_task_system_set_context (NULL);
+free_context:
+    hev_free (context);
+exit:
+    return -1;
 }
 
 EXPORT_SYMBOL void
 hev_task_system_fini (void)
 {
-#ifdef ENABLE_MEMALLOC_SLICE
     HevMemoryAllocator *allocator;
-#endif
-#ifdef ENABLE_PTHREAD
-    HevTaskSystemContext *default_context = pthread_getspecific (key);
-#endif
+    HevTaskSystemContext *context = pthread_getspecific (key);
 
-    hev_task_timer_destroy (default_context->timer);
-    hev_task_io_reactor_destroy (default_context->reactor);
-    hev_free (default_context);
+    hev_task_timer_destroy (context->timer);
+    hev_task_io_reactor_destroy (context->reactor);
+    hev_free (context);
+    hev_task_system_set_context (NULL);
 
-#ifdef ENABLE_PTHREAD
-    pthread_setspecific (key, NULL);
-#else
-    default_context = NULL;
-#endif
-
-#ifdef ENABLE_MEMALLOC_SLICE
     allocator = hev_memory_allocator_set_default (NULL);
     if (allocator)
         hev_memory_allocator_unref (allocator);
-#endif
 }
 
 EXPORT_SYMBOL void
@@ -108,21 +129,3 @@ hev_task_system_run (void)
 {
     hev_task_system_schedule (HEV_TASK_RUN_SCHEDULER);
 }
-
-HevTaskSystemContext *
-hev_task_system_get_context (void)
-{
-#ifdef ENABLE_PTHREAD
-    return pthread_getspecific (key);
-#else
-    return default_context;
-#endif
-}
-
-#ifdef ENABLE_PTHREAD
-static void
-pthread_key_creator (void)
-{
-    pthread_key_create (&key, NULL);
-}
-#endif
