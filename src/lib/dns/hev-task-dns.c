43cc6fcf172ac3cf4ef8d4a074e8ad331d5816ea
hev <r@hev.cc>
2021-06-20 10:42:38 +0800

    HevTaskDNS: Return error while proxy is null.

--- src/lib/dns/hev-task-dns.c
+++ src/lib/dns/hev-task-dns.c
@@ -7,6 +7,8 @@
  ============================================================================
  */
 
+#include <errno.h>
+
 #include "lib/misc/hev-compiler.h"
 #include "lib/dns/hev-task-dns-proxy.h"
 #include "kern/core/hev-task-system-private.h"
@@ -31,12 +33,13 @@ hev_task_dns_getaddrinfo (const char *node, const char *service,
                           const struct addrinfo *hints, struct addrinfo **res)
 {
     HevTaskDNSProxy *proxy = hev_task_dns_proxy_get ();
-    int ret = -1;
 
-    if (proxy)
-        ret = hev_task_dns_proxy_getaddrinfo (proxy, node, service, hints, res);
+    if (!proxy) {
+        errno = EIO;
+        return EAI_SYSTEM;
+    }
 
-    return ret;
+    return hev_task_dns_proxy_getaddrinfo (proxy, node, service, hints, res);
 }
 
 EXPORT_SYMBOL int
@@ -45,11 +48,12 @@ hev_task_dns_getnameinfo (const struct sockaddr *addr, socklen_t addrlen,
                           socklen_t servicelen, int flags)
 {
     HevTaskDNSProxy *proxy = hev_task_dns_proxy_get ();
-    int ret = -1;
 
-    if (proxy)
-        ret = hev_task_dns_proxy_getnameinfo (
-            proxy, addr, addrlen, node, nodelen, service, servicelen, flags);
+    if (!proxy) {
+        errno = EIO;
+        return EAI_SYSTEM;
+    }
 
-    return ret;
+    return hev_task_dns_proxy_getnameinfo (proxy, addr, addrlen, node, nodelen,
+                                           service, servicelen, flags);
 }
