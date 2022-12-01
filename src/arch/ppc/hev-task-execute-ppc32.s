7e7a79b35ec07be4f4240b45f6be04c6651ba0ac
hev <r@hev.cc>
2022-12-01 21:58:18 +0800

    HevTask: Import task execute for ppc.

---
+++ src/arch/ppc/hev-task-execute-ppc32.s
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : hev-task-execute-ppc32.s
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2022 everyone.
+ Description :
+ ============================================================================
+ */
+
+    .globl hev_task_execute
+    .type hev_task_execute, @function
+
+hev_task_execute:
+    lwz   5, 0(3)
+    mflr  0
+    stw   1, -0x0c(5)
+    stw   0, -0x10(5)
+    addi  1, 5, -0x10
+
+    mtctr 4
+    bctrl
+
+    lwz   0, 0x00(1)
+    lwz   1, 0x0c(1)
+    mtlr  0
+    blr
+
+    .size hev_task_execute, . - hev_task_execute
---
+++ src/arch/ppc/hev-task-execute-ppc64.s
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : hev-task-execute-ppc64.s
+ Author      : Heiher <r@hev.cc>
+ Copyright   : Copyright (c) 2022 everyone.
+ Description :
+ ============================================================================
+ */
+
+    .globl hev_task_execute
+    .type hev_task_execute, @function
+
+hev_task_execute:
+    ld    5, 0(3)
+    mflr  0
+    std   1, -0x08(5)
+    std   0, -0x10(5)
+    addi  1, 5, -0x10
+
+    mtctr 4
+    bctrl
+
+    ld    0, 0x00(1)
+    ld    1, 0x08(1)
+    mtlr  0
+    blr
+
+    .size hev_task_execute, . - hev_task_execute
